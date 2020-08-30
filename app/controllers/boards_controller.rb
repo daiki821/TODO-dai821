e76956a6f6da11bddfad4709a7ea962e34020c66
daiki <morilr.tabletennis@gmail.com>
2020-08-30 11:20:14 +0900

    createアクションを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,18 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
+
 end
