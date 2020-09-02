94e63b1f90ad56451ba35a5c25968bbd84dfc52c
daiki <morilr.tabletennis@gmail.com>
2020-09-02 23:29:10 +0900

    boardにおいてflashを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,8 +10,9 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'boardを作成できました'
     else
+      flash.now[:error] = 'boradの作成に失敗しました'
       render :new
     end
   end
