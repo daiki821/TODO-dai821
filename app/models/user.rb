e349925aed93ef447c181fc1efde4c1cb1bf0e0d
daiki <morilr.tabletennis@gmail.com>
2020-09-08 18:18:11 +0900

    taskモデルを作成しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+  has_many :tasks, dependent: :destroy
   has_one :profile, dependent: :destroy
+
 
   delegate :age, :gender,:birthday, to: :profile, allow_nil: true
 
