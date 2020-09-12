44aa4724e7e8c540de4f9bafacfe83dfe0876b58
daiki <morilr.tabletennis@gmail.com>
2020-09-12 16:57:08 +0900

    createアクションにboard_idを渡すようにコードを変更したらエラーが出てしまった

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   def new
-    board = Board.find(params[ :board_id])
-    @task = current_user.tasks.build
+    board = Board.find(params[:board_id])
+    @task = current_user.tasks.build(board_id: board)
+    
   end
 
   def create
-    board = Board.find(params[ :board_id])
-    @task = current_user.tasks.build(task_params)
+    board = Board.find(params[:board_id])
+    @task = current_user.tasks.build(task_params, board_id: board)
     
     if @task.save
       redirect_to root_path, notice: 'taskを作成しました'
