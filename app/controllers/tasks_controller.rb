6541d1d866f7b4b04e3e5e60b2bb1eefab884623
daiki <morilr.tabletennis@gmail.com>
2020-09-12 12:35:29 +0900

    newとcreateを変更 boardの値をどのように@taskに入れたら良いかわかりません

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   def new
-    board = board.find(params[ :board_id])
+    board = Board.find(params[ :board_id])
     @task = current_user.tasks.build
   end
 
   def create
-    board = board.find(params[ :board_id])
+    board = Board.find(params[ :board_id])
     @task = current_user.tasks.build(task_params)
+    
     if @task.save
       redirect_to root_path, notice: 'taskを作成しました'
     else 
