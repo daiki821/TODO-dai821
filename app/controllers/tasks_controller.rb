3582f70122cf3848993981e74bd65c381de4f931
daiki <morilr.tabletennis@gmail.com>
2020-09-18 23:27:22 +0900

    indexアクション追加 board一覧のboardたちをリンクにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,20 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    board = Board.find(params[:board_id])
+    @tasks = board.tasks.all
+    
+  end
+
   def new
     board = Board.find(params[:board_id])
     @task = board.tasks.build(user_id: current_user.id)
-    
   end
 
   def create
     board = Board.find(params[:board_id])
     @task = board.tasks.build(task_params.merge(user_id: current_user.id))
-    
-
     
     if @task.save
       redirect_to root_path, notice: 'taskを作成しました'
