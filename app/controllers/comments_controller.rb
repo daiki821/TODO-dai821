6666399d51f522e29a87b641c2c3d142b1861b1b
daiki <morilr.tabletennis@gmail.com>
2020-09-26 10:25:20 +0900

    コメントを作成できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,29 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+
+  def new
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:task_id])
+    @comment = @task.comments.build(user_id: current_user.id)
+  end
+
+  def create
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:task_id])
+    @comment = @task.comments.build(comment_params.merge(user_id: current_user.id))
+
+    if @comment.save
+      redirect_to board_task_path(board_id: @task.board_id, id: @task.id), notice: 'commentを作成しました'
+    else
+      flash.now[:error] = 'commentが作成できませんでした'
+      render :new
+    end
+  end
+
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
