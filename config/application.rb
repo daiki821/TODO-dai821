a2db9eea07eff11027919ff997f7cee34600c855
daiki <morilr.tabletennis@gmail.com>
2020-09-30 02:06:55 +0900

    本番環境ではdotenvを読み込まないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module Todo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
+    
 
 
     # Settings in config/environments/* take precedence over those specified here.
